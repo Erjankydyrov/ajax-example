48b3e12603d57815d09afcda0bd5225b50196287
Erjankydyrov <erjankydyrov29@gmail.com>
2021-03-31 15:57:14 +0600

    corected App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import { useState } from 'react';
 import './App.css';
 import Comments from './components/Comments/Comments';
+import FullPost from './components/FullPost/FullPost';
 import Posts from './components/Posts/Posts';
 
 function App() {
+  const [selectedPost, setSelectedPost] = useState(0);
+  let fullPost = "Select a post!";
+  if (selectedPost == 0) {
+    fullPost = <FullPost id = {selectedPost} />
+  }
   return (
     <div className="App">
-      <Posts />
-      <Comments />
+      <Posts setSelectedPost={setSelectedPost} />
+      {selectedPost}
+      <FullPost id = {selectedPost}/>
     </div>
   );
 }
