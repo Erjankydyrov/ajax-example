d6aa2d51f7c780fc0f4be4ad10fd7c6fb73c77c0
Erjankydyrov <erjankydyrov29@gmail.com>
2021-03-31 15:23:18 +0600

    added component Comments

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Comments from './components/Comments/Comments';
 import Posts from './components/Posts/Posts';
 
 function App() {
   return (
     <div className="App">
-      <Posts />
+      <Comments />
     </div>
   );
 }
---
+++ src/components/Comments/Comments.js
@@ -0,0 +1,29 @@
+import classes from "./Comments.module.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const Comments = () => {
+
+    const [comments, setComments] = useState([]);
+
+    useEffect(() => {
+        axios.get('https://jsonplaceholder.typicode.com/comments')
+          .then((response) => {
+            setComments(response.data.slice(0, 10));
+          });
+      }, []);
+    
+      const results = comments.map((comment) => (
+        <article>
+            <h2>{comment.name}</h2>
+            <p>{comment.body}</p>
+        </article>
+      ))
+    return ( 
+        <div className = {classes.Comments}>
+            {results}
+        </div>
+    );
+}
+ 
+export default Comments;
