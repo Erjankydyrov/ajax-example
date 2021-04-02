29195c66f65828b61ab3a4398fed7c0bdf40f4b3
Erjankydyrov <erjankydyrov29@gmail.com>
2021-04-02 15:35:26 +0600

    added new component

---
+++ src/components/Pictures/Pictures.js
@@ -0,0 +1,9 @@
+import classes from "./Pictures.module.css";
+
+const Pictures = ({ albumId }) => {
+    return ( 
+        <div className = {classes.Pictures}></div>
+    );
+}
+ 
+export default Pictures;
