9ff53d2cf03d38ef57acb1dfeb5bff34ec42eea2
Erjankydyrov <erjankydyrov29@gmail.com>
2021-04-02 14:45:01 +0600

    corected mistake in Author

--- src/components/Author/Author.js
+++ src/components/Author/Author.js
@@ -1,11 +1,23 @@
+import axios from "axios";
 import { useState } from "react";
+import { useEffect } from "react";
 import classes from "./Author.module.css";
 
 const Author = ({ userid }) => {
+    
+    const [email, setEmail] = useState("Loading");
+
+    useEffect(() => {
+        axios.get(`https://jsonplaceholder.typicode.com/users/${userid}`)
+            .then((response) => {
+                const emails = response.data;
+            setEmail(emails.email)
+        });
+    }, [userid]);
 
     return ( 
         <div className = {classes.Author}>
-            {userid}
+            {email}
         </div>
     );
 }
