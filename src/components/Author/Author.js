6e59621558e1e694aa450c0374237aa3ca1674bf
Erjankydyrov <erjankydyrov29@gmail.com>
2021-04-02 14:14:12 +0600

    added new component

---
+++ src/components/Author/Author.js
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import classes from "./Author.module.css";
+
+const Author = ({ user }) => {
+    const [auto, setAuto] = useState()
+    return ( 
+        <div className = {classes.Author}>
+
+        </div>
+    );
+}
+
+export default Author;
